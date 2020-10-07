febe3151651f1b970aad693ec4e904a974d68d6e
delaMorena <martadmdo@gmail.com>
2020-10-07 09:56:31 +0000

    css aplicado correctamente y casi listo para empezar con js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 require('./style.css');
 
 window.onload = function() { 
-    console.log('Hello world')
+/* You should generate a random number between 1 and 4 to pick a 
+suit and another random number 
+between 0 and 12 to pick a card number.
+ */ 
+    var getRandomNumber = Math.floor((Math.random() * 4) + 1);
+    var getRandomSuit = Math.floor(Math.random() * 12);
+
+
+    
 }
